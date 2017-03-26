30a61523539d713ef5914f61a21967408ac5ec3c
Jonathan Sudiaman <jsudiaman@gmail.com>
2017-03-26 12:52:59 -0400

    Improve cornhole detection (still far from perfect)

--- hsvRange.py
+++ hsvRange.py
@@ -1,7 +1,7 @@
 """
 Use this to get HSV ranges for cropped images (e.g. a beanbag by itself)
 
-Usage: python hsvRange.py --image image_file [--alpha error_tolerance]
+Usage: python hsvRange.py --image image_file [--tolerance error_tolerance]
 """
 import numpy as np
 import argparse
@@ -10,22 +10,22 @@ import cv2
 # Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
-ap.add_argument("-a", "--alpha", default=5, help="Error tolerance [0, 50]")
+ap.add_argument("-t", "--tolerance", default=5, help="Error tolerance [0, 50]")
 args = vars(ap.parse_args())
 
 # Print HSV bounds
 image = cv2.imread(args['image'])
-alpha = int(args['alpha'])
-if not 0 <= alpha <= 50:
-    raise ValueError("Alpha must be between 0 and 50.")
+tol = int(args['tolerance'])
+if not 0 <= tol <= 50:
+    raise ValueError("Tolerance must be between 0 and 50.")
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 hue = [channels[0] for sublist in hsv for channels in sublist]
 sat = [channels[1] for sublist in hsv for channels in sublist]
 val = [channels[2] for sublist in hsv for channels in sublist]
-lo_hue = np.percentile(hue, alpha)
-hi_hue = np.percentile(hue, 100 - alpha)
-lo_sat = np.percentile(sat, alpha)
-hi_sat = np.percentile(sat, 100 - alpha)
-lo_val = np.percentile(val, alpha)
-hi_val = np.percentile(val, 100 - alpha)
+lo_hue = np.percentile(hue, tol)
+hi_hue = np.percentile(hue, 100 - tol)
+lo_sat = np.percentile(sat, tol)
+hi_sat = np.percentile(sat, 100 - tol)
+lo_val = np.percentile(val, tol)
+hi_val = np.percentile(val, 100 - tol)
 print "bounds = ([%i, %i, %i], [%i, %i, %i])" % (lo_hue, lo_sat, lo_val, hi_hue, hi_sat, hi_val)
--- stateEstimation.py
+++ stateEstimation.py
@@ -9,6 +9,7 @@ lower_red = ([0, 70, 50], [10, 255, 255])
 upper_red = ([170, 70, 50], [180, 255, 255])
 blue_beanbags = ([100, 120, 0], [140, 255, 255])
 board = ([103, 31, 120], [105, 47, 135])
+cornhole = ([21, 38, 96], [29, 58, 142])
 
 # Beanbag values
 min_beanbag_area = 100
@@ -18,9 +19,9 @@ min_ch_radius = 30
 max_ch_radius = 50
 
 
-def annotate(filename):
+def analyze(filename):
     """
-    Annotate an image file using color detection algorithm.
+    Scan the source image for beanbags, board, and cornhole. Detected objects are annotated using rectangles or circles.
 
     :param filename: location of the source file
     :return: (image, red_rects, blue_rects, board_rect, cornhole)
@@ -44,20 +45,21 @@ def annotate(filename):
     board_rect = draw_board(image, board_mask)
 
     # Find the cornhole.
-    cornhole = draw_cornhole(image, board_rect)
+    circle_mask = cv2.inRange(hsv, np.array(cornhole[0], dtype="uint8"), np.array(cornhole[1], dtype="uint8"))
+    cornhole_circ = draw_cornhole(image, circle_mask, board_rect)
 
     # Return the image and its annotations.
-    return image, red_rects, blue_rects, board_rect, cornhole
+    return image, red_rects, blue_rects, board_rect, cornhole_circ
 
 
 def draw_beanbags(image, mask, color):
     """
-    Draws bounding boxes onto an image from a mask.
+    Draws bounding boxes around beanbags.
 
     :param image: image to annotate
     :param mask: mask used to find contours
     :param color: color of the bounding boxes (B,G,R)
-    :return: List of (x, y, w, h) tuples where (x, y) is the left corner, w is the width, and h is the height of the rectangle.
+    :return: List of (x, y, w, h) tuples where (x, y) is the top-left corner, w is the width, and h is the height of each rectangle.
     """
     rects = []
     contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -71,11 +73,11 @@ def draw_beanbags(image, mask, color):
 
 def draw_board(image, mask):
     """
-    Draws bounding box onto an image from a mask.
+    Draws bounding box around the board.
 
     :param image: image to annotate
-    :param mask: mask used to find contour
-    :return: (x, y, w, h) tuple where (x, y) is the left corner, w is the width, and h is the height of the rectangle.
+    :param mask: mask used to find contours
+    :return: (x, y, w, h) tuple where (x, y) is the top-left corner, w is the width, and h is the height of the rectangle.
     """
     contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) == 0:
@@ -85,9 +87,50 @@ def draw_board(image, mask):
     return x, y, w, h
 
 
-def draw_cornhole(image, board_rect=None):
+def draw_cornhole(image, mask, board_rect=None):
     """
-    Draws the cornhole.
+    Draws the cornhole using color filtering.
+
+    :param image: image to annotate
+    :param mask: mask used to find contours
+    :param board_rect: rectangular boundaries of the board
+    :return: (x, y, r) where (x, y) is the center and r is the radius of the circle.
+    """
+    board_x, board_y, board_width, board_height = board_rect
+    contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Discard contours outside the board
+    if board_rect is not None:
+        cs_in_board = []
+        for c in contours:
+            M = cv2.moments(c)
+            if M["m00"] > 0:
+                x = int(M["m10"] / M["m00"])
+                y = int(M["m01"] / M["m00"])
+                if board_x <= x <= board_x + board_width and board_y <= y <= board_y + board_height:
+                    cs_in_board.append(c)
+        contours = cs_in_board
+
+    while len(contours) > 0:
+        # Enclosing circle around the contours
+        (x, y), r = cv2.minEnclosingCircle(np.concatenate(contours))
+        x, y, r = int(x), int(y), int(r)
+
+        # Ensure that circle is reasonably sized
+        if not min_ch_radius <= r <= max_ch_radius:
+            break
+
+        # Circle looks good, so draw it
+        cv2.circle(image, (x, y), r, (0, 255, 255), 4)
+        return x, y, r
+
+    # Fallback to Hough transform
+    return draw_cornhole2(image, board_rect)
+
+
+def draw_cornhole2(image, board_rect=None):
+    """
+    Draws the cornhole using circular Hough transform.
 
     :param image: image to annotate
     :param board_rect: rectangular boundaries of the board
@@ -108,8 +151,30 @@ def draw_cornhole(image, board_rect=None):
             board_x, board_y, board_width, board_height = board_rect
             if not (board_x <= x <= board_x + board_width and board_y <= y <= board_y + board_height):
                 continue
-        cv2.circle(image, (x, y), r, (0, 255, 0), 4)
+        cv2.circle(image, (x, y), r, (0, 255, 255), 4)
         return x, y, r
+
+
+def center(rect):
+    """
+    Calculate the center of a rectangle using the midpoint formula.
+
+    :param rect: (x, y, w, h) tuple where (x, y) is the top-left corner, w is the width, and h is the height of the rectangle
+    :return: (x, y) tuple where (x, y) is the center of the rectangle.
+    """
+    x, y, w, h = rect
+    return float(x + x + w) / 2, float(y + y + h) / 2
+
+
+def error(exp, real):
+    """
+    Calculate percent error of an experimental value.
+
+    :param exp: experimental value
+    :param real: precise value
+    :return: Percent error err such that 0 <= err <= 100.
+    """
+    return float(abs(exp - real)) / abs(real) * 100
 
 
 if __name__ == '__main__':
@@ -123,31 +188,37 @@ if __name__ == '__main__':
 
     # UI
     while index <= total_imgs:
-        image, red_rects, blue_rects, board_rect, cornhole = annotate("dataset/img/%04d.jpg" % index)
+        image, red_rects, blue_rects, board_rect, cornhole_circ = analyze("dataset/img/%04d.jpg" % index)
         cv2.imshow("State Estimator", image)
 
         try:
+            # This is somewhat experimental
+            # TODO Eventually, we should get to the point where we are printing our own version of dataSet.json
+
             # Compare to data set
             data = dataset[index]
-            error = lambda exp, real: float(abs(exp - real)) / real * 100
 
             # Board
             ds_board = data['board']
-            expWidth, expHeight = board_rect[2], board_rect[3]
-            realWidth, realHeight = ds_board['size']['width'], ds_board['size']['height']
+            exp_board_x, exp_board_y = center(board_rect)
+            real_board_x, real_board_y = ds_board['center']
+            exp_board_width, exp_board_height = board_rect[2], board_rect[3]
+            real_board_width, real_board_height = ds_board['size']['width'], ds_board['size']['height']
             print "File: %04d.jpg" % index
             print "==== BOARD ===="
-            print "Width: %d (Error: %d%%)" % (expWidth, error(expWidth, realWidth))
-            print "Height: %d (Error: %d%%)" % (expHeight, error(expHeight, realHeight))
+            print "x: %d (Error: %d%%)" % (exp_board_x, error(exp_board_x, real_board_x))
+            print "y: %d (Error: %d%%)" % (exp_board_x, error(exp_board_y, real_board_y))
+            print "Width: %d (Error: %d%%)" % (exp_board_width, error(exp_board_width, real_board_width))
+            print "Height: %d (Error: %d%%)" % (exp_board_height, error(exp_board_height, real_board_height))
 
             # Cornhole
             ds_hole = data['board']['hole']
-            expX, expY, expRadius = cornhole
-            realX, realY, realRadius = ds_hole['center'][0], ds_hole['center'][1], ds_hole['radius']
+            exp_ch_x, exp_ch_y, exp_radius = cornhole_circ
+            real_ch_x, real_ch_y, real_radius = ds_hole['center'][0], ds_hole['center'][1], ds_hole['radius']
             print "=== CORNHOLE ==="
-            print "x: %d (Error: %d%%)" % (expX, error(expX, realX))
-            print "y: %d (Error: %d%%)" % (expY, error(expY, realY))
-            print "radius: %d (Error: %d%%)" % (expRadius, error(expRadius, realRadius))
+            print "x: %d (Error: %d%%)" % (exp_ch_x, error(exp_ch_x, real_ch_x))
+            print "y: %d (Error: %d%%)" % (exp_ch_y, error(exp_ch_y, real_ch_y))
+            print "radius: %d (Error: %d%%)" % (exp_radius, error(exp_radius, real_radius))
         except:
             print "==== EXCEPTION ===="
             traceback.print_exc()
